1b7e1f65848285ef7e139227f958b3c26ac883e1
Lidia Butkovic <lbutkovi1@student.unipu.hr>
2026-08-11 09:39:34 +0200

    Dodana stranica Moje rezervacije

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import DashboardView from '../views/DashboardView.vue'
+import ReservationsView from '../views/ReservationsView.vue'
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', name: 'login', component: LoginView },
   { path: '/register', name: 'register', component: RegisterView },
   { path: '/dashboard', name: 'dashboard', component: DashboardView },
+  { path: '/reservations', name: 'reservations', component: ReservationsView },
 ]
 
 const router = createRouter({
